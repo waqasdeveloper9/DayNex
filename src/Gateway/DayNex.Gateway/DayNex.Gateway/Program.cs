b9735090e28f7f373ecb8c8b4cddb3e479db76ff
Waqas Mumtaz <waqas.mumtaz@synergy-it.com>
2026-06-14 23:53:38 +0500

    Add Ocelot Settings

--- src/Gateway/DayNex.Gateway/DayNex.Gateway/Program.cs
+++ src/Gateway/DayNex.Gateway/DayNex.Gateway/Program.cs
@@ -1,15 +1,32 @@
+using Serilog;
+
 namespace DayNex.Gateway
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+            Log.Information("DayNex Gateway starting up...");
+            try
+            {
 
-            app.MapGet("/", () => "Hello World!");
+                var builder = WebApplication.CreateBuilder(args);
+                var app = builder.Build();
 
-            app.Run();
+                app.MapGet("/", () => "Hello World!");
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
     }
 }
